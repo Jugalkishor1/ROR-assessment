012749d29044c7b8571e7b2ad5576c4388583559
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-06-01 14:03:45 +0530

    Add comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
-  def new
+  def create
+    debugger
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to posts_path, notice: 'Comment was successfully added.'
+    else
+      redirect_to posts_path, alert: 'Something went wrong.'
+    end
   end
 
-  def create
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
